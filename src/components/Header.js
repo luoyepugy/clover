6aa076647de2b58c3ff269ece78c7c11fdc03450
张晗 <zhanghan@zhanghandeMacBook-Pro.local>
2017-08-17 16:08:46 +0800

    首页基本完成

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,22 @@ var Header = React.createClass({
     render: function() {
         return (
             <div>
-                <div>这是header</div>
+                <div className="header">
+                    <div className="nav">
+                        <div className="logo"><a title="首页" href="#/"><img src={require('../images/logo.png')} alt="logo" /></a></div>
+                        <ul>
+                            <li><a title="精选" href="#/recommend">精选</a></li>
+                            <li><a title="风光" href="#/scenery">风光</a></li>
+                            <li><a title="微距" href="#/macro">微距</a></li>
+                            <li><a title="人物" href="#/person">人物</a></li>
+                            <li><a title="联系我们" href="#/contact_us">联系我们</a></li>
+                            <li className="sound"><span title="音频 开/关" className="soundOff j-sound"></span></li>
+                        </ul>
+                    </div>
+                </div>
+                <audio id="audio" preload="auto" loop="loop">
+                    <source src={require('file-loader!../media/audio.mp3')} type="audio/mpeg" />
+                </audio>
             </div>
         );
     }
--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,5 @@
 
 
-
 import React from 'react';
 import Header from '../components/Header.js';
 
@@ -9,8 +8,18 @@ var Index = React.createClass({
         return (
             <div>
                 <Header></Header>
-                <div><a href="#/">返回首页</a></div>
-                <div>这是详情页</div>
+                <div className="content">
+                    <div className="slider-box" id="sliderBox">
+                        <ul className="slider-list">
+                            <li className="slider-item"><img src={require('../images/index_1.jpg')} alt="1" /></li>
+                            <li className="slider-item"><img src={require('../images/index_2.jpg')} alt="2" /></li>
+                            <li className="slider-item"><img src={require('../images/index_3.jpg')} alt="3" /></li>
+                            <li className="slider-item"><img src={require('../images/index_4.jpg')} alt="4" /></li>
+                        </ul>
+                        <div className="slider-links"></div>
+                        <div className="slider-dots"></div>
+                    </div>
+                </div>
             </div>
         );
     }
